790ec27893ade6cabce0cb7e7b5e4b1da26dd146
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-27 14:58:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,10 @@ require_relative 'config/environment'
 require 'pry'
 
 class App < Sinatra::Base
-
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
 
 get '/' do
   erb :index
